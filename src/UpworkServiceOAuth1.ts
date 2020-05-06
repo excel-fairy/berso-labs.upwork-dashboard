571b9723d2cc91ada7ddfd091133e92b65491816
Remi Somdecoste-Lespoune <saponace@gmail.com>
2020-05-06 16:08:30 +0200

    feat: retrieve financial and time reports from upwork API

--- src/UpworkServiceOAuth1.ts
+++ src/UpworkServiceOAuth1.ts
@@ -6,13 +6,41 @@ type TokenAndVerifier = {
 }
 
 class UpworkServiceOAuth1 {
-    private static UPWORK_ACCESS_TOKEN_URL = "http://term.ie/oauth/example/access_token.php";
-    private static UPWORK_REQUEST_TOKEN_URL = "http://term.ie/oauth/example/request_token.php";
-    // TODO: Do not use authorization URL (UPWORK does not redirect to app callback URL)
-    private static UPWORK_REQUEST_AUTHORIZATION_URL = "http://google.com";
-    private static UPWORK_DATA_URL = "http://term.ie/oauth/example/echo_api.php";
-    private static CONSUMER_KEY = "key";
-    private static CONSUMER_SECRET = "secret";
+    private static UPWORK_REQUEST_TOKEN_URL = "https://www.upwork.com/api/auth/v1/oauth/token/request";
+    private static UPWORK_ACCESS_TOKEN_URL = "https://www.upwork.com/api/auth/v1/oauth/token/access";
+    private static UPWORK_REQUEST_AUTHORIZATION_URL = "http://www.upwork.com/services/api/auth";
+
+    private static USER_BASIC_INFO_URL = "https://www.upwork.com/api/auth/v1/info.json";
+    private static USER_INFO_URL = "https://www.upwork.com/api/hr/v2/users/me.json";
+    private static USER_TEAMS_URL = "https://www.upwork.com/api/hr/v2/teams.json";
+    private static USER_COMPANIES_URL = "https://www.upwork.com/api/hr/v2/companies.json";
+
+    private static userId = "xxx";
+    private static CONSUMER_KEY = "xxx";
+    private static CONSUMER_SECRET = "xxx";
+
+
+    private static dateStart = "2019-10-01";
+    private static dateEnd = "2019-10-10";
+
+
+    private static TIME_REPORT_URL = `https://www.upwork.com/gds/timereports/v1/providers/${UpworkServiceOAuth1.userId}/hours`;
+    private static TIME_REPORT_PARAM = {
+        tq: `SELECT worked_on, hours, team_name, assignment_name, charges
+        WHERE worked_on >= '${UpworkServiceOAuth1.dateStart}' AND worked_on <= '${UpworkServiceOAuth1.dateEnd}'
+        ORDER BY worked_on`,
+        tqx: "json",
+    };
+
+    // TODO: TEAM_ID is retrieved from USER_TEAMS_URL. Current user does not seem to be able to access financial records for its team.
+    private static FINANCIAL_REPORT_URL = `https://www.upwork.com/gds/finreports/v2/financial_accounts/xxx`;
+    // Fun thing: condition on dates requires to surround dates with single quotes for financial report, whereas time repport supports double quotes
+    private static FINANCIAL_REPORT_PARAM = {
+        tq: `SELECT date, type, subtype, description, buyer_team_name, amount, assignment_name 
+        WHERE date >= '${UpworkServiceOAuth1.dateStart}' AND date <= '${UpworkServiceOAuth1.dateEnd}'
+        ORDER BY date`,
+        tqx: "json",
+    };
 
     constructor() {
     }
@@ -36,6 +64,7 @@ class UpworkServiceOAuth1 {
 
         service.setOAuthVersion("1.0");
         service.setParamLocation("uri-query");
+        service.setMethod("POST");
         return service;
     }
 
@@ -46,25 +75,25 @@ class UpworkServiceOAuth1 {
     static showSidebar = () => {
         const service = UpworkServiceOAuth1.getUpworkService();
         if (!service.hasAccess()) {
+            console.log("Not connected to Upwork. Requesting user to authenticate");
             const authorizationUrl = service.authorize();
             const template = HtmlService.createTemplate(
                 '<a href="<?= authorizationUrl ?>" target="_blank">Authorize</a>. ' +
                 'Reopen the sidebar when the authorization is complete.');
-            // TODO: add form for verifier (and maybe confirmation of token) + call authCallback on form submission
             template.authorizationUrl = authorizationUrl;
             const page = template.evaluate();
             SpreadsheetApp.getUi().showSidebar(page);
         } else {
             // TODO: close sidebar
+            console.log("Application already have access. No need to re-authorize")
+            UpworkServiceOAuth1.logTokens();
         }
     }
 
     /**
-     * google-apps-script-oauth1 does not provide a way to have a callback URL that does not expire.
-     * Will then have to manually enter the OAuth verifier in the sheet (via HTML popup for instance)
+     * Handle OAuth1 callback
      * @param request The OAuth object
      */
-        // TODO: will maybe have to modify OAuth1.handleCallBack() if upwork authorization page does not show the OAuth token
     static authCallback = (request: TokenAndVerifier) => {
         const service = UpworkServiceOAuth1.getUpworkService();
         const isAuthorized = service.handleCallback(request);
@@ -75,10 +104,37 @@ class UpworkServiceOAuth1 {
         }
     }
 
-    static makeRequest = () => {
+    static getUserInfo = () => {
         const service = UpworkServiceOAuth1.getUpworkService();
-        const response = service.fetch(UpworkServiceOAuth1.UPWORK_DATA_URL);
-        console.log(response);
+        const response = service.fetch(UpworkServiceOAuth1.USER_COMPANIES_URL);
+        UpworkServiceOAuth1.logResponse("User info", UpworkServiceOAuth1.handleResponse(response));
+    }
+
+    static getTimeReport = () => {
+        const service = UpworkServiceOAuth1.getUpworkService();
+        const response = service.fetch(UpworkServiceOAuth1.TIME_REPORT_URL, {
+                payload: UpworkServiceOAuth1.TIME_REPORT_PARAM,
+            }
+        );
+        UpworkServiceOAuth1.logResponse("Time report", UpworkServiceOAuth1.handleResponse(response));
+    }
+
+    static getFinancialReport = () => {
+        const service = UpworkServiceOAuth1.getUpworkService();
+        const response = service.fetch(UpworkServiceOAuth1.FINANCIAL_REPORT_URL, {
+                payload: UpworkServiceOAuth1.FINANCIAL_REPORT_PARAM,
+            }
+        );
+        UpworkServiceOAuth1.logResponse("Financial report", UpworkServiceOAuth1.handleResponse(response));
+    }
+
+    private static handleResponse = (response: any) => {
+        return JSON.parse(response.getContentText());
+    }
+
+    private static logResponse = (indication: string, response: any) => {
+
+        console.log(`indication :`, JSON.stringify(response, null, 2));
     }
 
     static logTokens = () => {
