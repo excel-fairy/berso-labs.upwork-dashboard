cfbbc5de65b57a7aa091bd4bc3d446af39b1a822
Remi Somdecoste-Lespoune <saponace@gmail.com>
2020-05-07 17:01:44 +0200

    wip: code tidying

--- src/UpworkServiceOAuth1.ts
+++ src/UpworkServiceOAuth1.ts
@@ -10,37 +10,16 @@ class UpworkServiceOAuth1 {
     private static UPWORK_ACCESS_TOKEN_URL = "https://www.upwork.com/api/auth/v1/oauth/token/access";
     private static UPWORK_REQUEST_AUTHORIZATION_URL = "http://www.upwork.com/services/api/auth";
 
-    private static USER_BASIC_INFO_URL = "https://www.upwork.com/api/auth/v1/info.json";
-    private static USER_INFO_URL = "https://www.upwork.com/api/hr/v2/users/me.json";
-    private static USER_TEAMS_URL = "https://www.upwork.com/api/hr/v2/teams.json";
-    private static USER_COMPANIES_URL = "https://www.upwork.com/api/hr/v2/companies.json";
-
-    private static userId = "xxx";
-    private static CONSUMER_KEY = "xxx";
-    private static CONSUMER_SECRET = "xxx";
+    private static freelancerId = "xxx";
+    private static freelancerFinancialAccountId = 0;
+    private static financialAccountId = 0;
+    private static oauthConsummerKey = "xxx";
+    private static oauthConsummerSecret = "xxx";
 
 
     private static dateStart = "2019-10-01";
     private static dateEnd = "2019-10-10";
 
-
-    private static TIME_REPORT_URL = `https://www.upwork.com/gds/timereports/v1/providers/${UpworkServiceOAuth1.userId}/hours`;
-    private static TIME_REPORT_PARAM = {
-        tq: `SELECT worked_on, hours, team_name, assignment_name, charges
-        WHERE worked_on >= '${UpworkServiceOAuth1.dateStart}' AND worked_on <= '${UpworkServiceOAuth1.dateEnd}'
-        ORDER BY worked_on`,
-        tqx: "json",
-    };
-
-    // TODO: TEAM_ID is retrieved from USER_TEAMS_URL. Current user does not seem to be able to access financial records for its team.
-    private static FINANCIAL_REPORT_URL = `https://www.upwork.com/gds/finreports/v2/financial_accounts/xxx`;
-    // Fun thing: condition on dates requires to surround dates with single quotes for financial report, whereas time repport supports double quotes
-    private static FINANCIAL_REPORT_PARAM = {
-        tq: `SELECT date, type, subtype, description, buyer_team_name, amount, assignment_name 
-        WHERE date >= '${UpworkServiceOAuth1.dateStart}' AND date <= '${UpworkServiceOAuth1.dateEnd}'
-        ORDER BY date`,
-        tqx: "json",
-    };
 
     constructor() {
     }
@@ -53,8 +32,8 @@ class UpworkServiceOAuth1 {
             .setAuthorizationUrl(UpworkServiceOAuth1.UPWORK_REQUEST_AUTHORIZATION_URL)
 
             // Set the consumer key and secret.
-            .setConsumerKey(UpworkServiceOAuth1.CONSUMER_KEY)
-            .setConsumerSecret(UpworkServiceOAuth1.CONSUMER_SECRET)
+            .setConsumerKey(UpworkServiceOAuth1.oauthConsummerKey)
+            .setConsumerSecret(UpworkServiceOAuth1.oauthConsummerSecret)
 
             // Set the name of the callback function in the script referenced
             // above that should be invoked to complete the OAuth flow.
@@ -105,24 +84,45 @@ class UpworkServiceOAuth1 {
     }
 
     static getUserInfo = () => {
+        const userBasicInfoUrl = "https://www.upwork.com/api/auth/v1/info.json";
+        const userInfoUrl = "https://www.upwork.com/api/hr/v2/users/me.json";
+        const userTeamsUrl = "https://www.upwork.com/api/hr/v2/teams.json";
+        const userCompaniesUrl = "https://www.upwork.com/api/hr/v2/companies.json";
         const service = UpworkServiceOAuth1.getUpworkService();
-        const response = service.fetch(UpworkServiceOAuth1.USER_COMPANIES_URL);
+        const response = service.fetch(userBasicInfoUrl);
         UpworkServiceOAuth1.logResponse("User info", UpworkServiceOAuth1.handleResponse(response));
     }
 
     static getTimeReport = () => {
+        const url = `https://www.upwork.com/gds/timereports/v1/providers/${UpworkServiceOAuth1.freelancerId}/hours`;
+        const params = {
+            tq: `SELECT worked_on, hours, team_name, assignment_name, charges
+                WHERE worked_on >= '${UpworkServiceOAuth1.dateStart}' AND worked_on <= '${UpworkServiceOAuth1.dateEnd}'
+                ORDER BY worked_on`,
+            tqx: "json",
+        };
+
         const service = UpworkServiceOAuth1.getUpworkService();
-        const response = service.fetch(UpworkServiceOAuth1.TIME_REPORT_URL, {
-                payload: UpworkServiceOAuth1.TIME_REPORT_PARAM,
+        const response = service.fetch(url, {
+                payload: params,
             }
         );
         UpworkServiceOAuth1.logResponse("Time report", UpworkServiceOAuth1.handleResponse(response));
     }
 
     static getFinancialReport = () => {
+        const url = `https://www.upwork.com/gds/finreports/v2/financial_accounts/${UpworkServiceOAuth1.freelancerFinancialAccountId}`;
+        // Fun thing: condition on dates requires to surround dates with single quotes for financial report, whereas time report supports double quotes
+        const params = {
+            tq: `SELECT date, type, subtype, description, buyer_team_name, amount, assignment_name 
+                WHERE date >= '${UpworkServiceOAuth1.dateStart}' AND date <= '${UpworkServiceOAuth1.dateEnd}'
+                ORDER BY date`,
+            tqx: "json",
+        };
+
         const service = UpworkServiceOAuth1.getUpworkService();
-        const response = service.fetch(UpworkServiceOAuth1.FINANCIAL_REPORT_URL, {
-                payload: UpworkServiceOAuth1.FINANCIAL_REPORT_PARAM,
+        const response = service.fetch(url, {
+                payload: params,
             }
         );
         UpworkServiceOAuth1.logResponse("Financial report", UpworkServiceOAuth1.handleResponse(response));
